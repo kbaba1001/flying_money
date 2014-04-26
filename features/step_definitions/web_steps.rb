c1446ea462001d7dcc448821d2c5881c240dfeef
kbaba1001 <kbaba1001@gmail.com>
2014-04-26 15:51:47 +0900

    支出を新規登録するシナリオを書いた
    warden によるログインのステップも作成した

---
+++ features/step_definitions/web_steps.rb
@@ -0,0 +1,27 @@
+前提(/^次のユーザーが存在する:$/) do |table|
+  table.hashes.each do |hash|
+    User.create!(
+      email:                 hash[:email],
+      password:              hash[:password],
+      password_confirmation: hash[:password],
+    )
+  end
+end
+
+前提(/^ユーザー "(.*?)" でログインする$/) do |email|
+  user = User.find_by(email: email)
+  login_as(user, scope: :user)
+end
+
+もし(/^"(.*?)" に "(.*?)" と入力する$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^"(.*?)" をクリックする$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^次の表を表示すること:$/) do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
