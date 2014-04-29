ffe718a238eec2e75f50d577fbd385f77fda50e3
kbaba1001 <kbaba1001@gmail.com>
2014-04-29 18:17:20 +0900

    Add user_steps.rb

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -1,17 +1,3 @@
-前提(/^次のユーザーが存在する:$/) do |table|
-  table.hashes.each do |hash|
-    attrs = {email: hash[:email]}
-    attrs[:password] = hash[:password] if hash[:password]
-
-    create(:user, attrs)
-  end
-end
-
-前提(/^ユーザー "(.*?)" としてサインインする$/) do |email|
-  user = User.find_by(email: email)
-  login_as(user, scope: :user)
-end
-
 前提(/^"(.*?)" を表示する$/) do |page_name|
   path =
     case page_name
