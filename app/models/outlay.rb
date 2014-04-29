2738b33823d8d1c442983ab9b40c9cfb35935d3b
kbaba1001 <kbaba1001@gmail.com>
2014-04-29 17:02:51 +0900

    最大文字数のバリデーションエラーを設定した

--- app/models/outlay.rb
+++ app/models/outlay.rb
@@ -1,5 +1,6 @@
 class Outlay < ActiveRecord::Base
   belongs_to :user
 
-  validates :amount, presence: true
+  validates :amount, presence: true, length: {maximum: 10}
+  validates :note, length: {maximum: 140}
 end
