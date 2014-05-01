2af8275ea1f940d0f82d9eb5bed10e68086ffd4c
kbaba1001 <kbaba1001@gmail.com>
2014-05-01 13:16:31 +0900

    cosmetics

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < ApplicationController
   def show
-    @outlay = current_user.outlays.build
+    @outlay  = current_user.outlays.build
     @outlays = current_user.outlays.created.includes(:expense_item)
   end
 end
