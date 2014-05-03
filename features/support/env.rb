b9434440fc7db1cd7d8ca977e843ba28ea6ee019
kbaba1001 <kbaba1001@gmail.com>
2014-05-03 16:25:28 +0900

    setting for simple_cov

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,6 +6,11 @@
 
 require 'cucumber/rails'
 
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start
+end
+
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.
