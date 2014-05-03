ba74264d055e99f911ebc8c0cd9aa8df348ed158
kbaba1001 <kbaba1001@gmail.com>
2014-05-03 18:56:41 +0900

    oops

--- app/controllers/outlays_controller.rb
+++ app/controllers/outlays_controller.rb
@@ -5,7 +5,7 @@ class OutlaysController < ApplicationController
     if @outlay.save
       redirect_to root_path
     else
-      @outlays = current_user.outlays.created.includes(:expense_item)
+      @outlay_months = current_user.outlays.created.includes(:expense_item).group_by {|outlay| outlay.created_at.beginning_of_month }
       render 'dashboards/show'
     end
   end
