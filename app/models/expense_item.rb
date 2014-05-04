7c7d2f7ef8b67d749e8fe60b9d19ea079f34dcb2
kbaba1001 <kbaba1001@gmail.com>
2014-05-04 23:37:33 +0900

    費目を追加できるようにした
    TODO
    
    - 費目設定ページを表示するときに display_order の初期値を text_field に入れておきたい

--- app/models/expense_item.rb
+++ app/models/expense_item.rb
@@ -3,8 +3,10 @@ class ExpenseItem < ActiveRecord::Base
   has_many :outlays
 
   validates :name, presence: true, uniqueness: {scope: :user_id}
-  validates :display_order, presence: true
+  validates :display_order, presence: true, uniqueness: {scope: :user_id}, numericality: {only_integer: true, greater_than: 0}
   validates :user_id, presence: true
+
+  before_validation :set_default_display_order, if: 'display_order.blank?'
 
   DEFAULTS = %w(食費 交通費 会食費 教育費 娯楽費 医療費 雑費)
 
@@ -12,7 +14,7 @@ class ExpenseItem < ActiveRecord::Base
 
   class << self
     def create_default_expense_items!(user)
-      DEFAULTS.each_with_index do |name, display_order|
+      DEFAULTS.each.with_index(1) do |name, display_order|
         create!(
           name:          name,
           display_order: display_order,
@@ -20,5 +22,11 @@ class ExpenseItem < ActiveRecord::Base
         )
       end
     end
+  end
+
+  private
+
+  def set_default_display_order
+    self.display_order = ExpenseItem.where(user: user).maximum(:display_order) + 1
   end
 end
