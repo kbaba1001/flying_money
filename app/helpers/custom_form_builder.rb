07d2102838ea4f0940303c1b8939708040c199a6
kbaba1001 <kbaba1001@gmail.com>
2014-05-07 23:12:04 +0900

    支出入力フォームのデザイン対応
    必須項目に「*」をつけるなどして、わかるようにしたい

--- app/helpers/custom_form_builder.rb
+++ app/helpers/custom_form_builder.rb
@@ -2,6 +2,6 @@ class CustomFormBuilder < ActionView::Helpers::FormBuilder
   def error_messages_on(field_name)
     return unless error_messages = object.errors.full_messages_for(field_name).presence
 
-    @template.content_tag(:div, error_messages.join(' '), class: 'error')
+    @template.content_tag(:small, error_messages.join(' '), class: 'error')
   end
 end
