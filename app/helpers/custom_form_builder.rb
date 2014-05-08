80836959b1883e1760cd7e294dfa4723a8237649
kbaba1001 <kbaba1001@gmail.com>
2014-05-08 22:17:14 +0900

    form_forのオプションを省略できるようにした

--- app/helpers/custom_form_builder.rb
+++
@@ -1,7 +0,0 @@
-class CustomFormBuilder < ActionView::Helpers::FormBuilder
-  def error_messages_on(field_name)
-    return unless error_messages = object.errors.full_messages_for(field_name).presence
-
-    @template.content_tag(:small, error_messages.join(' '), class: 'error')
-  end
-end
