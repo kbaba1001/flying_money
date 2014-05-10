479913d9ed651c539b06d1992771021a201fd2bd
kbaba1001 <kbaba1001@gmail.com>
2014-05-10 14:31:24 +0900

    アコーディオンのidはhelperで生成するようにした

--- app/helpers/outlay_helper.rb
+++ app/helpers/outlay_helper.rb
@@ -1,5 +1,9 @@
-module NumberHelper
+module OutlayHelper
   def number_to_yen(number)
     number_to_currency(number, precision: 0, unit: '円', format: '%n%u')
+  end
+
+  def accordion_css_id(month)
+    "accordion-outlay-#{month.strftime('%Y-%m')}"
   end
 end
