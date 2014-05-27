1ea40b9c767bfd2fca71b91e515921710b1dbf95
kbaba1001 <kbaba1001@gmail.com>
2014-05-27 22:11:50 +0900

    月毎の支出合計額を表示するロジックを変更した

--- app/helpers/outlay_helper.rb
+++ app/helpers/outlay_helper.rb
@@ -2,8 +2,4 @@ module OutlayHelper
   def number_to_yen(number)
     number_to_currency(number, precision: 0, unit: '円', format: '%n%u')
   end
-
-  def accordion_css_id(month)
-    "accordion-outlay-#{month.strftime('%Y-%m')}"
-  end
 end
