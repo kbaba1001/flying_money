efb3a3372dabe6eef3ccb59cb6534976201fba4b
kbaba1001 <kbaba1001@gmail.com>
2014-06-03 21:49:41 +0900

    RailsAdminが行うFormの拡張が使えないのでモンキーパッチを外した

---
+++ app/helpers/custom_form_builder.rb
@@ -0,0 +1,7 @@
+class CustomFormBuilder < ActionView::Helpers::FormBuilder
+  def error_messages_on(field_name)
+    return unless error_messages = object.errors.full_messages_for(field_name).presence
+
+    @template.content_tag(:small, error_messages.join(' '), class: 'error')
+  end
+end
